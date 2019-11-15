23f5999e30cb99fe267cb80f3a2cab04d71c8fe3
Eagle19243 <eagle19243@gmail.com>
2019-11-15 15:02:03 -0500

    Add Test for AuthPage

---
+++ src/__test__/pages/AuthPage.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import AuthPage from '../../components/pages/AuthPage';
+import SiteNav from '../../components/partials/SiteNav';
+
+describe('AuthPage', () => {
+  const render = () => {
+    const wrapper = shallow(<AuthPage />);
+    return wrapper;
+  };
+
+  it('should render AuthPage', () => {
+    const authPage = render();
+    expect(authPage).toHaveLength(1);
+  });
+
+  it('should have SiteNav component', () => {
+    const authPage = render();
+    expect(authPage.find(SiteNav)).toHaveLength(1);
+  });
+
+  it('should have div component with className "content"', () => {
+    const authPage = render();
+    expect(authPage.find('div').last().hasClass('content')).toEqual(true);
+  });
+});
